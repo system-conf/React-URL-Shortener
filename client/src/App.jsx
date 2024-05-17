ec72c1e28a3c66344032c7120a1d0a9ecbab1365
system.conf <suleyman.talha.duman0@gmail.com>
2024-05-17 05:20:19 +0300

    client update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,62 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [longUrl, setLongUrl] = useState('');
-  const [shortUrl, setShortUrl] = useState('');
+  const [longUrl, setLongUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setError('');
+    setError("");
 
     try {
-      const response = await axios.post('https://url-shorter-react-server.vercel.app/shorten', { longUrl });
+      const response = await axios.post(
+        "https://url-shorter-react-server.vercel.app/shorten",
+        { longUrl }
+      );
       setShortUrl(response.data.shortUrl);
     } catch (error) {
-      setError('Kısaltma işlemi sırasında bir hata oluştu. Lütfen tekrar deneyin.');
+      setError(
+        "Kısaltma işlemi sırasında bir hata oluştu. Lütfen tekrar deneyin."
+      );
     } finally {
       setLoading(false);
     }
   };
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(shortUrl);
+  };
+
   return (
     <div className="App">
-      <h1>URL Kısaltıcı</h1>
+      <div className="copy">Created by <a href="https://suleymantalha.dev/" target="_blank">system.conf </a>/ <a href="https://github.com/system-conf/urlShorterReact" target="_blank">GitHub</a> / License</div>
+      <div className="baslik">URL SHORTENER</div>
       <form onSubmit={handleSubmit}>
-        <input
+        <input className="box"
           type="text"
           value={longUrl}
           onChange={(e) => setLongUrl(e.target.value)}
           placeholder="Uzun URL'i girin"
         />
-        <button type="submit" disabled={loading}>{loading ? 'Kısaltılıyor...' : 'Kısalt'}</button>
+   &nbsp; &nbsp;
+        <button type="submit" disabled={loading}>
+          {loading ? "Kısaltılıyor..." : "Kısalt"}
+        </button>
       </form>
       {error && <p>{error}</p>}
       {shortUrl && (
         <div>
-          <p>Kısaltılmış URL: <a href={shortUrl} target="_blank" rel="noopener noreferrer">{shortUrl}</a></p>
+          <p>
+            <a href={shortUrl} target="_blank" rel="noopener noreferrer">
+              {shortUrl}
+            </a>
+          </p>
+          <button onClick={handleCopy}>Kopyala</button>
         </div>
       )}
     </div>
